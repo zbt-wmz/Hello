ddec3470b1717a4e8059e6a97c6eb4fa7e738e94
zbt-wmz <29625@qq.com>
2020-04-25 23:03:03 +0800

    change 01

--- HelloWorld .java
+++ HelloWorld .java
@@ -1,11 +1,11 @@
-public class HelloWorld   #如果有 public 类的话，类名必须和文件同名，注意大小写  
+class HelloWorld
 {  
-  #Java 入口程序，程序从此入口  
+
   public static void main(String[] args)  
   {  
-  #向控制台打印一条语句  
+
     System.out.println("Hello,World!");
         System.out.println("My name is zbt!");
-        System.out.println("I am coming!");
+        System.out.println("Nice to me you");
         }
 }
